aeae9678327094607d90f8a2b2e54f312c98e0a4
Ryan <ryanxdlee@gmail.com>
2024-08-07 18:12:31 +0800

    make login and signup pages nice

--- src/views/Login.js
+++ src/views/Login.js
@@ -1,56 +1,74 @@
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { useState } from "react";
-import { Button, Container, Form } from "react-bootstrap";
+import { Button, Col, Container, Form, Row } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
 import { auth } from "../firebase";
 
 const Login = () => {
-  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
   const navigate = useNavigate();
 
   return (
     <Container>
-      <h1 className="my-3">Login</h1>
+      <h1 className="my-3">Login to AniGram</h1>
       <Form>
-        <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control
-            type="email"
-            placeholder="Enter email"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-          />
+        <Form.Group className="mb-3" controlId="user">
+          <Row>
+            <Col lg="1" className="col-12">
+              <Form.Label className="pt-2">Email:</Form.Label>
+            </Col>
+            <Col>
+              <Form.Control
+                type="text"
+                placeholder="Enter the email address associated with your account"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </Col>
+          </Row>
         </Form.Group>
-
-        <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control
-            type="password"
-            placeholder="Password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          <a href="/signup">Sign up for an account</a>
+        <Form.Group className="mb-3" controlId="password">
+          <Row>
+            <Col lg="1" className="col-12">
+              <Form.Label className="pt-2">Password:</Form.Label>
+            </Col>
+            <Col>
+              <Form.Control
+                type="password"
+                placeholder="Enter your password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </Col>
+          </Row>
         </Form.Group>
-        <Button
-          variant="primary"
-          onClick={async () => {
-            setError("");
-            const notEmpty = username && password;
-            if (notEmpty) {
-              try {
-                await signInWithEmailAndPassword(auth, username, password);
-                navigate("/");
-              } catch (error) {
-                setError(error.message);
+        <Row>
+          <a href="/signup">
+            Don't have an AniGram account yet? Create one here!
+          </a>
+        </Row>
+        <Row className="justify-content-end mx-0 my-2">
+          <Button
+            variant="primary"
+            className="col-12 col-md-2"
+            onClick={async () => {
+              setError("");
+              if (email && password) {
+                try {
+                  await signInWithEmailAndPassword(auth, email, password);
+                  console.log(`{email} logged in`);
+                  navigate("/");
+                } catch (error) {
+                  setError(error.message);
+                }
               }
-            }
-          }}
-        >
-          Login
-        </Button>
+            }}
+          >
+            Login
+          </Button>
+        </Row>
       </Form>
       <p>{error}</p>
     </Container>
--- src/views/Signup.js
+++ src/views/Signup.js
@@ -1,10 +1,11 @@
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
 import { useState } from "react";
-import { Button, Container, Form } from "react-bootstrap";
+import { Button, Col, Container, Form, Row } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
 import { auth } from "../firebase";
 
 const SignUp = () => {
+  const [email, setEmail] = useState("");
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
@@ -12,46 +13,98 @@ const SignUp = () => {
 
   return (
     <Container>
-      <h1 className="my-3">Sign up for an account</h1>
+      <h1 className="my-3">Create a new AniGram Account</h1>
       <Form>
-        <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control
-            type="email"
-            placeholder="Enter email"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-          />
+        <Form.Group className="mb-3" controlId="email">
+          <Row>
+            <Col lg="1" className="col-12">
+              <Form.Label className="pt-2">Email:</Form.Label>
+            </Col>
+            <Col>
+              <Form.Control
+                type="email"
+                placeholder="Enter the email address to be associated with your account"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </Col>
+          </Row>
         </Form.Group>
-
-        <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control
-            type="password"
-            placeholder="Password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          <a href="/login">Have an existing account? Login here.</a>
+        <Form.Group className="mb-3" controlId="username">
+          <Row>
+            <Col lg="1" className="col-12">
+              <Form.Label className="pt-2">Username:</Form.Label>
+            </Col>
+            <Col>
+              <Form.Control
+                type="text"
+                placeholder="Enter a username for your account"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+              />
+              <Form.Text className="text-muted">
+                Username cannot include spaces. Spaces will be replaced with
+                underscores by default. If left empty or invalid, a username
+                will be generated from your email.
+              </Form.Text>
+            </Col>
+          </Row>
         </Form.Group>
-        <Button
-          variant="primary"
-          onClick={async () => {
-            setError("");
-            const notEmpty = username && password;
-            if (notEmpty) {
-              try {
-                await createUserWithEmailAndPassword(auth, username, password);
-                navigate("/");
-                console.log(`${username} created`);
-              } catch (error) {
-                setError(error);
+        <Form.Group className="mb-3" controlId="password">
+          <Row>
+            <Col lg="1" className="col-12">
+              <Form.Label className="pt-2">Password:</Form.Label>
+            </Col>
+            <Col>
+              <Form.Control
+                type="password"
+                placeholder="Enter a password for your account"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </Col>
+          </Row>
+        </Form.Group>
+        <Row>
+          <a href="/login">
+            Have an existing AniGram account? Login here instead!
+          </a>
+        </Row>
+        <Row className="justify-content-end mx-0 my-2">
+          <Button
+            variant="primary"
+            className="col-12 col-md-2"
+            onClick={async () => {
+              setError("");
+              if (email && password) {
+                try {
+                  await createUserWithEmailAndPassword(auth, email, password);
+                  if (!username) {
+                    await updateProfile(auth.currentUser, {
+                      displayName: email.split("@")[0],
+                    });
+                  } else if (username.match(" ")) {
+                    await updateProfile(auth.currentUser, {
+                      displayName: username.replaceAll(" ", "_"),
+                    });
+                  } else {
+                    await updateProfile(auth.currentUser, {
+                      displayName: username,
+                    });
+                  }
+                  // console.log(
+                  //   `User: ${auth.currentUser.displayName} created\nEmail: ${email}`
+                  // );
+                  navigate("/");
+                } catch (error) {
+                  setError(error.message);
+                }
               }
-            }
-          }}
-        >
-          Sign Up
-        </Button>
+            }}
+          >
+            Create Account
+          </Button>
+        </Row>
       </Form>
       <p>{error}</p>
     </Container>
