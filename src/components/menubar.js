79853fddd7bda0741dfac33916e86aa58f4f4105
Ryan <ryanxdlee@gmail.com>
2024-08-07 21:14:16 +0800

    update menubar

--- src/components/menubar.js
+++ src/components/menubar.js
@@ -1,15 +1,50 @@
-import { Container, Nav, Navbar } from "react-bootstrap";
+import { Button, Container, Image, Nav, Navbar } from "react-bootstrap";
 import { signOut } from "firebase/auth";
 import { auth } from "../firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useNavigate } from "react-router-dom";
 
 const Menubar = () => {
+  const [user] = useAuthState(auth);
+  const navigate = useNavigate();
+
+  const NavOptions = () => {
+    if (user) {
+      return (
+        <>
+          <Nav.Link href="/search">New Post</Nav.Link>
+          <Nav.Link
+            onClick={() => {
+              signOut(auth);
+              navigate("/login");
+            }}
+          >
+            Sign Out
+          </Nav.Link>
+          <Nav.Link href={`/user/${user.uid}`}>
+            <Image
+              src={user.photoURL}
+              style={{
+                height: "2rem",
+                width: "2rem",
+                borderRadius: "50%",
+                objectFit: "cover",
+              }}
+            />
+          </Nav.Link>
+        </>
+      );
+    } else {
+      return <Nav.Link href="/login">Login</Nav.Link>;
+    }
+  };
+
   return (
     <Navbar variant="light" bg="light">
       <Container>
         <Navbar.Brand href="/">AniGram</Navbar.Brand>
         <Nav>
-          <Nav.Link href="/search">New Post</Nav.Link>
-          <Nav.Link onClick={() => signOut(auth)}>🚪</Nav.Link>
+          <NavOptions />
         </Nav>
       </Container>
     </Navbar>
